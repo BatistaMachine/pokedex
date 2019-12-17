5dc41fd7364f7c68acd56a12b5fc532b41802542
BatistaMachine <33598815+BatistaMachine@users.noreply.github.com>
2019-12-17 12:21:28 +0100

    everything work

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import spinner from '../layout/spinner.gif';
+import spinner from "../layout/spinner.gif";
 
 const Sprite = styled.img`
   width: 5em;
@@ -40,9 +40,9 @@ const StyledLink = styled(Link)`
 
 export default class PokemonCard extends Component {
   state = {
-    name: '',
-    imageUrl: '',
-    pokemonIndex: '',
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
     imageLoading: true,
     toManyRequests: false
   };
@@ -50,7 +50,7 @@ export default class PokemonCard extends Component {
   componentDidMount() {
     const { name, url } = this.props;
 
-    const pokemonIndex = url.split('/')[url.split('/').length - 2];
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
     //const imageUrl = `./sprites/pokemon/${pokemonIndex}.png`;
     const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
 
@@ -66,7 +66,7 @@ export default class PokemonCard extends Component {
             {this.state.imageLoading ? (
               <img
                 src={spinner}
-                style={{ width: '5em', height: '5em' }}
+                style={{ width: "10em", height: "1em" }}
                 className="card-img-top rounded mx-auto d-block mt-2"
               />
             ) : null}
@@ -77,10 +77,10 @@ export default class PokemonCard extends Component {
               onError={() => this.setState({ toManyRequests: true })}
               style={
                 this.state.toManyRequests
-                  ? { display: 'none' }
+                  ? { display: "none" }
                   : this.state.imageLoading
                   ? null
-                  : { display: 'block' }
+                  : { display: "block" }
               }
             />
             {this.state.toManyRequests ? (
@@ -94,9 +94,9 @@ export default class PokemonCard extends Component {
               <h6 className="card-title">
                 {this.state.name
                   .toLowerCase()
-                  .split(' ')
+                  .split(" ")
                   .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-                  .join(' ')}
+                  .join(" ")}
               </h6>
             </div>
           </Card>
